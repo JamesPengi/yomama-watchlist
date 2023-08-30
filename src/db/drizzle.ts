b8f1c24064bb2e246a73b208639ebd865f0c3389
Joshua Fernandes <fernandes.josh@gmail.com>
2023-08-30 18:54:41 +0530

    refactor: updated deprecated types

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,17 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 import { env } from "~/env.mjs";
-import type { InferModel } from "drizzle-orm";
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
 
 const queryClient = postgres(env.DATABASE_URL);
 export const db = drizzle(queryClient, { schema });
 
-export type Title = InferModel<typeof schema.titles, "select">;
-export type Title__Insert = InferModel<typeof schema.titles, "insert">;
+export type Title = InferSelectModel<typeof schema.titles>;
+export type Title__Insert = InferInsertModel<typeof schema.titles>;
 
-export type User = InferModel<typeof schema.users, "select">;
+export type User = InferSelectModel<typeof schema.users>;
 
-export type TitleToUser__Insert = InferModel<
-  typeof schema.titlesToUsers,
-  "insert"
->;
+export type TitleToUser__Insert = InferInsertModel<typeof schema.titlesToUsers>;
