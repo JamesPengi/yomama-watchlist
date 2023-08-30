d3d81c486eb7683c266bea4aa636522feaf5c798
Joshua Fernandes <fernandes.josh@gmail.com>
2023-08-30 11:13:47 +0530

    refactor: moved quickadd to appdir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { QuickAdd } from "./_components/QuickAdd";
+
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <main className="container mx-auto mt-10 space-y-10">Content</main>;
+  return (
+    <main className="container mx-auto mt-10 space-y-10">
+      <QuickAdd />
+    </main>
+  );
 }
