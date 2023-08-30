75b02d4fdee2ff0040da90d21b4e2646eb38aa82
Joshua Fernandes <fernandes.josh@gmail.com>
2023-08-30 11:00:39 +0530

    refactor: added base pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { type Metadata } from "next";
+import React from "react";
+import Provider from "./_trpc/Provider";
+import { twMerge } from "tailwind-merge";
+import { Rubik } from "next/font/google";
+
+const fontSans = Rubik({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Yo Mama's Watchlist",
+  description: "A watchlist for friends",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body
+        className={twMerge(
+          "flex min-h-screen flex-col space-x-5 font-sans",
+          fontSans.variable
+        )}
+      >
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export const dynamic = "force-dynamic";
+
+export default function Home() {
+  return <main className="container mx-auto mt-10 space-y-10">Content</main>;
+}
