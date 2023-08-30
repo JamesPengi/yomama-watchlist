d9f0fac8056d74b34b80bbed49c145c5dbcd4ad1
Joshua Fernandes <fernandes.josh@gmail.com>
2023-08-30 19:33:55 +0530

    refactor: removed unecessary env files

--- src/env.mjs
+++ src/env.mjs
@@ -8,9 +8,8 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    CLERK_SECRET_KEY: z.string(),
     TMDB_API_KEY: z.string(),
-    DATABASE_URL: z.string().url()
+    DATABASE_URL: z.string().url(),
   },
 
   /**
@@ -18,9 +17,7 @@ export const env = createEnv({
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-  },
+  client: {},
 
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -28,8 +25,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     TMDB_API_KEY: process.env.TMDB_API_KEY,
     DATABASE_URL: process.env.DATABASE_URL,
   },
