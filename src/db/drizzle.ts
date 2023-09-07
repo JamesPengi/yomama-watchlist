ea5cf2f5679fdaffa2ff719d3495ef147edecf02
Joshua Fernandes <fernandes.josh@gmail.com>
2023-09-07 14:45:05 +0530

    fix: changed to pool connection
    db transactions were not compatable with drizzle's neon-http library

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,10 +1,12 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import * as schema from "./schema";
 import { env } from "~/env.mjs";
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
 
-const queryClient = neon(env.DATABASE_URL);
+neonConfig.fetchConnectionCache = true;
+
+const queryClient = new Pool({ connectionString: env.DATABASE_URL });
 export const db = drizzle(queryClient, { schema });
 
 export type Title = InferSelectModel<typeof schema.titles>;
