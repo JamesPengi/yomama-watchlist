4f4b243a4aa4ce6090766b8d4f3098df10423baf
Joshua Fernandes <fernandes.josh@gmail.com>
2023-09-20 15:15:29 +0530

    feat: sort search results by popularity

--- src/utils/generateResponse.ts
+++ src/utils/generateResponse.ts
@@ -88,44 +88,47 @@ export function generateSearchResponse(
 }
 
 export function generateSearchResults(tmdbData: tmdbGeneralQueryResult[]) {
-  return tmdbData.map((title) => {
-    let mediaType: tmdbMediaType = "movie";
-    let titleName = title.title;
-    let searchableTitle = title.original_title!;
-    let release_date = title.release_date!;
+  return tmdbData
+    .map((title) => {
+      let mediaType: tmdbMediaType = "movie";
+      let titleName = title.title;
+      let searchableTitle = title.original_title!;
+      let release_date = title.release_date!;
 
-    if (title.media_type === "tv") {
-      mediaType = "tv";
-      titleName = title.name!;
-      searchableTitle = title.original_name!;
-      if (
-        title.origin_country[0] === "JP" ||
-        title.origin_country[0] === "KR"
-      ) {
-        mediaType = "anime";
+      if (title.media_type === "tv") {
+        mediaType = "tv";
+        titleName = title.name!;
+        searchableTitle = title.original_name!;
+        if (
+          title.origin_country[0] === "JP" ||
+          title.origin_country[0] === "KR"
+        ) {
+          mediaType = "anime";
+        }
       }
-    }
 
-    let genre: tmdbGenreName = getReadableGenreName(title.genre_ids[0]!);
+      let genre: tmdbGenreName = getReadableGenreName(title.genre_ids[0]!);
 
-    if (genre === "Animation" && mediaType === "anime") {
-      if (title.genre_ids[1]) {
-        genre = getReadableGenreName(title.genre_ids[1]);
+      if (genre === "Animation" && mediaType === "anime") {
+        if (title.genre_ids[1]) {
+          genre = getReadableGenreName(title.genre_ids[1]);
+        }
       }
-    }
 
-    if (mediaType === "tv" || mediaType === "anime") {
-      release_date = title.first_air_date!;
-    }
+      if (mediaType === "tv" || mediaType === "anime") {
+        release_date = title.first_air_date!;
+      }
 
-    return {
-      title: titleName,
-      searchableTitle: searchableTitle,
-      mediaType: mediaType,
-      genre: genre,
-      releaseDate: release_date.slice(0, 4),
-      posterPath: title.poster_path,
-      overview: title.overview.slice(0, 300),
-    };
-  });
+      return {
+        title: titleName,
+        searchableTitle: searchableTitle,
+        mediaType: mediaType,
+        genre: genre,
+        releaseDate: release_date.slice(0, 4),
+        posterPath: title.poster_path,
+        overview: title.overview.slice(0, 300),
+        popularity: title.popularity,
+      };
+    })
+    .sort((a, b) => b.popularity - a.popularity);
 }
