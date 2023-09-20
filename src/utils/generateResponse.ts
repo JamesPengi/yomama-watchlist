c212af28baad048b3d743837babefa76a2f32c3e
Joshua Fernandes <fernandes.josh@gmail.com>
2023-09-20 15:54:58 +0530

    fix: added filter for person
    didn't realize that people are in the multi search. didn't expect them in the data fetch from api

--- src/utils/generateResponse.ts
+++ src/utils/generateResponse.ts
@@ -1,12 +1,7 @@
-import type {
-  getAllResponse,
-  getOneResponse,
-  searchOptionsResponse,
-} from "~/types/ApiResponses";
+import type { getAllResponse, getOneResponse } from "~/types/ApiResponses";
 import {
   getReadableGenreName,
   type getOneApiResponse,
-  type tmdbGeneralQueryResponse,
   type tmdbGenreName,
   type tmdbMediaType,
   type tmdbGeneralQueryResult,
@@ -43,62 +38,24 @@ export function generateGetOneResponse(
   };
 }
 
-export function generateSearchResponse(
-  tmdbData: tmdbGeneralQueryResponse
-): searchOptionsResponse[] {
-  const data = tmdbData.results.slice(0, 5);
-
-  return data.map((title) => {
-    let mediaType: tmdbMediaType = "movie";
-    let titleName = title.title;
-    let release_date = title.release_date!;
-
-    if (title.media_type === "tv") {
-      mediaType = "tv";
-      titleName = title.name!;
-      if (
-        title.origin_country[0] === "JP" ||
-        title.origin_country[0] === "KR"
-      ) {
-        mediaType = "anime";
-      }
-    }
-
-    let genre: tmdbGenreName = getReadableGenreName(title.genre_ids[0]!);
-
-    if (genre === "Animation" && mediaType === "anime") {
-      if (title.genre_ids[1]) {
-        genre = getReadableGenreName(title.genre_ids[1]);
-      }
-    }
-
-    if (mediaType === "tv" || mediaType === "anime") {
-      release_date = title.first_air_date!;
-    }
-
-    return {
-      tmdbId: String(title.id),
-      title: titleName,
-      mediaType: mediaType,
-      releaseDate: release_date.slice(0, 4),
-      genre: genre,
-      posterPath: title.poster_path,
-    };
-  });
-}
-
 export function generateSearchResults(tmdbData: tmdbGeneralQueryResult[]) {
-  return tmdbData
+  const data = tmdbData
+    .filter((title) => title.media_type !== "person")
     .map((title) => {
       let mediaType: tmdbMediaType = "movie";
-      let titleName = title.title;
-      let searchableTitle = title.original_title!;
-      let release_date = title.release_date!;
+      let genre: tmdbGenreName;
+      const titleName = title.title ? title.title : title.name!;
+      const searchableTitle = title.original_title
+        ? title.original_title
+        : title.original_name!;
+      const release_date = title.release_date
+        ? title.release_date
+        : title.first_air_date
+        ? title.first_air_date
+        : "";
 
       if (title.media_type === "tv") {
         mediaType = "tv";
-        titleName = title.name!;
-        searchableTitle = title.original_name!;
         if (
           title.origin_country[0] === "JP" ||
           title.origin_country[0] === "KR"
@@ -107,28 +64,31 @@ export function generateSearchResults(tmdbData: tmdbGeneralQueryResult[]) {
         }
       }
 
-      let genre: tmdbGenreName = getReadableGenreName(title.genre_ids[0]!);
-
-      if (genre === "Animation" && mediaType === "anime") {
-        if (title.genre_ids[1]) {
-          genre = getReadableGenreName(title.genre_ids[1]);
+      if (title.genre_ids[0]) {
+        genre = getReadableGenreName(title.genre_ids[0]!);
+        if (genre === "Animation" && mediaType === "anime") {
+          if (title.genre_ids[1]) {
+            genre = getReadableGenreName(title.genre_ids[1]);
+          }
         }
+      } else {
+        genre = "Unknown";
       }
 
-      if (mediaType === "tv" || mediaType === "anime") {
-        release_date = title.first_air_date!;
-      }
-
-      return {
+      const data = {
         title: titleName,
         searchableTitle: searchableTitle,
         mediaType: mediaType,
         genre: genre,
-        releaseDate: release_date.slice(0, 4),
+        releaseDate: release_date !== "" ? release_date.slice(0, 4) : "",
         posterPath: title.poster_path,
         overview: title.overview.slice(0, 300),
         popularity: title.popularity,
       };
+      console.log(data);
+      return data;
     })
     .sort((a, b) => b.popularity - a.popularity);
+  console.log(data);
+  return data;
 }
